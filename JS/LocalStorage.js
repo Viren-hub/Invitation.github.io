da51b4d49f9bf415ced261a0c2af9efc5b4c3cd0
Virendra Gayakwad <62784876+Viren-hub@users.noreply.github.com>
2021-05-11 20:46:16 +0530

    Initial Commit

---
+++ JS/LocalStorage.js
@@ -0,0 +1,20 @@
+let data = localStorage.getItem("invite") ? JSON.parse(localStorage.getItem("invite")) : []
+
+function edit() {
+
+    let newData = {
+        "MaleName": document.getElementById("male").value,
+        "FemaleName": document.getElementById("female").value,
+        "venue": document.getElementById("place").value,
+        "Date": document.getElementById("date").value,
+
+    }
+
+
+    data.push(newData);
+    localStorage.setItem("invite", JSON.stringify(data));
+    document.getElementById("Name1").innerHTML = newData.MaleName;
+    document.getElementById("Name2").innerHTML = newData.FemaleName;
+    document.getElementById("city").innerHTML = newData.venue;
+    document.getElementById("DateNow").innerHTML = newData.Date;
+}
